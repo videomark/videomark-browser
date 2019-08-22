48830f686d757552967d1f9ad31b09ddc185ce96
hATrayflood <h.rayflood@gmail.com>
2019-08-22 13:49:25 +0900

    improved patches for chromium 76

--- gen_sodium.sh
+++ gen_sodium.sh
@@ -7,7 +7,7 @@ rm -rf ${RESDIR}
 mkdir -p ${RESDIR}
 
 cd ${CURDIR}/sodium.js && ./node_modules/.bin/webpack --config webpack.prod.js
-sed -e 's/"/\\"/g' ${CURDIR}/sodium.js/dist/sodium.js | tr -d "\r\n" > ${RESDIR}/sodium.js
+cp ${CURDIR}/sodium.js/dist/sodium.js ${RESDIR}/sodium.js
 
 cd ${CURDIR}/videomark-log-view && ./build-for-android.sh
 cp -r ${CURDIR}/videomark-log-view/build ${RESDIR}/sodium_result
